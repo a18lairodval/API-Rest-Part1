5150bfce0733f6b60d88ef5a5547a84efe2dc094
Laia <a18lairodval@insprelabes.cat>
2020-06-07 16:37:28 +0200

    post delete i put afegits i funcionals

--- src/Repository/PeliculasRepository.php
+++ src/Repository/PeliculasRepository.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Repository;
 
 use App\Entity\Peliculas;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @method Peliculas|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,11 +14,39 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PeliculasRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry,EntityManagerInterface $manager)
     {
         parent::__construct($registry, Peliculas::class);
+        $this->manager = $manager;
     }
 
+    public function savePelicula($nombre, $genero, $descripcion)
+    {
+        $newPeliculas = new Peliculas();
+
+        $newPeliculas
+            ->setNombre($nombre)
+            ->setGenero($genero)
+            ->setDescripcion($descripcion);
+
+        $this->manager->persist($newPeliculas);
+        $this->manager->flush();
+    }
+
+    public function updatePelicula(Peliculas $Peliculas): Peliculas
+    {
+        $this->manager->persist($Peliculas);
+        $this->manager->flush();
+
+        return $Peliculas;
+    }
+
+
+    public function removePelicula(Peliculas $Peliculas)
+    {
+        $this->manager->remove($Peliculas);
+        $this->manager->flush();
+    }
     // /**
     //  * @return Peliculas[] Returns an array of Peliculas objects
     //  */
